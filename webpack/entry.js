e603b7b44db3b958d60ee89259fd33fd4072834c
kewagi <kewagi@icloud.com>
2018-05-13 20:12:25 +0200

    Document.ready

--- webpack/entry.js
+++ webpack/entry.js
@@ -4,4 +4,6 @@ import 'jquery';
 import 'popper.js';
 import 'bootstrap-material-design';
 import './main.scss';
-$('body').bootstrapMaterialDesign();
+$( document ).ready(function() {
+    $('body').bootstrapMaterialDesign();
+});
